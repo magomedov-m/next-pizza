f06b280f67dc98b8267f84ec8a033a7cce7e2e29
magomedov-m <magomedov.dgu@gmail.com>
2024-10-03 18:16:07 +0300

    init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const nunito = Nunito({
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="ru">
+      <body className={`${nunito.variable} ${nunito.variable} antialiased`}>
+        <main className="min-h-screen">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import "./globals.css";
 
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  );
+  return <h1>Главная</h1>;
 }
