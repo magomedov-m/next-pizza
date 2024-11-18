d0559bdb91b5491682780d8c20d18689cbaea4c4
magomedov-m <magomedov.dgu@gmail.com>
2024-11-18 15:40:34 +0300

    Пытался настроить подключение к серверу

--- app/shared/SearchInputField.tsx
+++ app/shared/SearchInputField.tsx
@@ -6,14 +6,17 @@ import { cn } from "@/lib/utils";
 import { useClickAway } from "react-use";
 import Link from "next/link";
 import { Api } from "@/sevices/api-client";
+import { Product } from "@prisma/client";
 
 interface Props {
   className?: string;
 }
 
 const SearchInputField: React.FC<Props> = ({ className }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
   const [focused, setFocused] = useState(false);
+
   const ref = useRef(null);
 
   useClickAway(ref, () => {
@@ -21,38 +24,60 @@ const SearchInputField: React.FC<Props> = ({ className }) => {
   });
 
   useEffect(() => {
-    Api.products.search('')
-  })
-
+    Api.products.search(searchQuery).then(items => {
+      console.log('результаты поиска:', items)
+      setProducts(items);
+    });
+  }, [searchQuery]);
+  
+  console.log(products, "this is products");
   return (
     <>
-      {focused && <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30"></div>}
+      {focused && (
+        <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30"></div>
+      )}
+
+      <div
+        className={cn(
+          "flex rounded-2xl flex-1 justify-between relative h-11 z-30",
+          className
+        )}
+      >
+        <Search className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-400" />
+        <input
+          className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
+          type="text"
+          placeholder="Найти пиццу..."
+          onFocus={() => setFocused(true)}
+          ref={ref}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
 
         <div
           className={cn(
-            "flex rounded-2xl flex-1 justify-between relative h-11 z-30",
-            className
+            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+            focused && "visible opacity-100 top-12"
           )}
         >
-          <Search className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-400" />
-          <input
-            className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
-            type="text"
-            placeholder="Найти пиццу..."
-            onFocus={() => setFocused(true)}
-            ref={ref}
-          />
-
-          <div className={cn('absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
-          focused && 'visible opacity-100 top-12')}>
-            <Link  className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10" href={'/product/1'}>
-              <img className="rounded-sm h-8 w-8" src="https://media.dodostatic.net/image/r:292x292/11EE7D6108E3A1C9952CD3A7F39A4D02.avif" alt="Пицца 1" width={32} height={32}/>
-              <div>
-                Пицца 1
-              </div>
+          {Array.isArray(products) && products.map((product) => (
+            <Link
+              key={product.id}
+              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+              href={`/products/${product.id}`}
+            >
+              <img
+                className="rounded-sm h-8 w-8"
+                src={product.imageUrl}
+                alt={product.name}
+                width={32}
+                height={32}
+              />
+              <div>{product.name}</div>
             </Link>
-          </div>
+          ))}
         </div>
+      </div>
     </>
   );
 };
--- sevices/products.ts
+++ sevices/products.ts
@@ -1,10 +1,11 @@
 import { axiosInstance } from "./instance";
 import { Product } from "@prisma/client";
+import { ApiRoutes } from "./constants";
 
-export const search = async (query: string) => {
-  const { data } = await axiosInstance.get<Product>("/products/search", {
-    params: { query },
-  });
+export const search = async (query: string): Promise<Product[]> => {
+  const { data } = await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {
+    params: { query }
+  })
 
   return data;
-};
+}
