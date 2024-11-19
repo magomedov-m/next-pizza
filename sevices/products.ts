57eb168d4ca68bd3fc1b0d3ff99041194fc672db
magomedov-m <magomedov.dgu@gmail.com>
2024-11-19 05:35:14 +0300

    отлов ошибок try/catch

--- sevices/products.ts
+++ sevices/products.ts
@@ -3,9 +3,14 @@ import { Product } from "@prisma/client";
 import { ApiRoutes } from "./constants";
 
 export const search = async (query: string): Promise<Product[]> => {
-  const { data } = await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {
-    params: { query }
-  })
-
-  return data;
+  try {
+    // return (await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, { params: { query } })).data;
+    const resp = await axiosInstance.get(ApiRoutes.SEARCH_PRODUCTS, {params: {query}});
+    console.log('Полный ответ от API:', resp)
+    console.log(resp.data, 'resp.data.products')
+    return resp.data.products
+  } catch (error) {
+    console.error('Ошибка при выполнении запроса поиска продуктов:', error);
+    return [];
+  }
 }
