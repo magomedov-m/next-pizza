9e52fa53cb52fe71a017242b5dbf8a5d28c9ef82
magomedov-m <magomedov.dgu@gmail.com>
2024-12-24 11:50:18 +0300

    начал создавать новый компонент для отображения товаров

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/prisma/prismaClient"
 import { notFound } from "next/navigation"
+import { Container } from "@/app/shared/Container"
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
     const product = await prisma.product.findFirst({ where: {id: Number(id)}})
@@ -8,5 +9,7 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
         return notFound()
     }
 
-    return <p>Product: {product?.name}</p>
+    return <Container className="flex flex-col my-10">
+        <ProductImage src={product.imageUrl} />
+    </Container>
 }
