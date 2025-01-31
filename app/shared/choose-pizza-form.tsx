3728a9546b5658dc43d404531abed42899ba665d
magomedov-m <magomedov.dgu@gmail.com>
2025-01-31 13:46:42 +0300

    .

--- app/shared/choose-pizza-form.tsx
+++ app/shared/choose-pizza-form.tsx
@@ -42,7 +42,7 @@ const ChoosePizzaForm: React.FC<Props> = ({
   const pizzaPrice = items?.find((item) => item.pizzaType === type && item.size === size)!.price;
   const totalIngredientsPrice = ingredients.filter((ingredient) => selectedIngredients.has(ingredient.id)).reduce((acc, ingredient) => acc + ingredient.price, 0);
   
-  const totalPrice = pizzaPrice + totalIngredientsPrice;
+  const totalPrice = pizzaPrice || 0 + totalIngredientsPrice;
 
   return (
     <div className={cn(className, "flex flex-1")}>
